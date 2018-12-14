71f35d14bcb5d9f3d9809473b7fc0b189cfebe9d
Filip Jankovikj <jankovikjfilip@gmail.com>
2018-12-14 13:07:29 +0100

    js

---
+++ c1.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+
+var ime = 'Bojan'; // string
+var brojNaKokoshki = 10; // int
+const pi = 3.14; // const float
+var visina = 1.73; // float
+var vrneSneg = false; // bool
+
+var neznam = true;
+var kilaza = 90.1;
+var imeAkademija = 'Semos Akademija';
+var brStudenti = 13;
+
+console.log (vrneSneg);
+
+vrneSneg = true;
+
+console.log(vrneSneg);
+
+// pi = 2.8; // ova ne raboti!!! konstantite ne se promelnivi
+// console.log (pi);
+
+var edinechniNavodnici = 'test';
+var dupliNavodnici = "test";
+var nakoseniNavodnici = `test`;
+
+console.log(edinechniNavodnici, dupliNavodnici, nakoseniNavodnici);
