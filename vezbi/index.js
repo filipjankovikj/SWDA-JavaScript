443a2b1f3be7ea00cee75b5cf28aee54b5c3f45e
Filip Jankovikj <43668823+filipjankovikj@users.noreply.github.com>
2019-02-15 12:32:01 +0100

    Add files via upload

---
+++ vezbi/index.js
@@ -0,0 +1,94 @@
+// functions
+
+// definiranje na funkcija
+function sobiranje(a, b) { // function sobiranje() - function signature
+    c= a + b;
+    console.log(c);
+}
+
+// function body
+// {
+//     c = a + b;
+// console.log(c);
+// }
+
+
+sobiranje(3, 6);
+
+
+// funkcii sto ne vrakjaat vrednost se vikaat "void" funckii
+
+
+function odzemanje(a, b) {
+    var c = a - b;
+    return c;
+}
+
+var d = odzemanje(4, 9);
+console.log(d);
+
+
+// funkcija parNepar
+
+
+function parNepar(a) {
+    if(a % 2 == 0){
+        return 'par';
+    } else {
+        return 'nepar';
+    }
+}
+
+var res = parNepar(2);
+console.log(res); // par
+
+res = parNepar(7);
+console.log(res); // nepar
+
+
+//
+
+
+function ubavMesec(mesec) {
+    if(mesec.length % 2 == 0){
+        return 'da';
+    } else {
+        return 'ne';
+    }
+}
+
+res = ubavMesec('Dekemvri'); // Dekemvri ima paren broj na bukvi i zatoa e ubav mesec
+console.log(res); // da
+
+res = ubavMesec('Januari'); // Januari ima neparen broj na bukvi zatoa ne e ubavo mesec
+console.log(res); // ne
+
+
+//
+
+
+var delenje = function(a, b){
+    return a / b;
+}
+
+res = delenje(3, 6);
+console.log(res);
+
+
+// 
+
+
+var ploshtina = function(a, b) {
+    return a * b;
+}
+
+res = ploshtina(2, 13);
+console.log(res);
+
+res = ploshtina(57, 298);
+console.log(res);
+
+
+//
+
+
